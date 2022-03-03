0c809752ab754384cfc5b13282c652604ad86611
JLXSchaf <62465144+JLXSchaf@users.noreply.github.com>
2022-03-03 14:46:14 +0100

    add default policy

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,7 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddCors();
+builder.Services.AddCors(x =>
+{
+    x.AddDefaultPolicy(y =>
+    {
+        y.AllowAnyHeader();
+        y.AllowAnyMethod();
+        y.AllowAnyOrigin();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
